5a8c5a840ea0221d520dc10efd5bc71c5be8deba
angeloamoako <51134064+angeloamoako@users.noreply.github.com>
2023-05-31 12:09:00 +0200

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	src/app/app.component.html
    #	src/app/app.module.ts

---
+++ src/app/test-actor-component/test-actor-component.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators } from "@angular/forms";
+import { Apollo } from 'apollo-angular';
+import { GET_ACTORS, GET_FILMS } from '../graphql/graphql.queries';
+
+
+@Component({
+  selector: 'app-test-actor-component',
+  templateUrl: './test-actor-component.component.html',
+  styleUrls: ['./test-actor-component.component.css']
+})
+export class TestActorComponentComponent implements OnInit{
+  actors: any[] = [];
+  films: any[] = [];
+  error: any;
+
+  actorForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl('')
+  });
+
+  constructor(private apollo: Apollo) { }
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query: GET_ACTORS
+    }).valueChanges.subscribe(({ data, error }: any) => {
+        this.actors = data.actors;
+        console.log("Dati letti dal server GraphQL: ",data);
+        this.error = error;
+      }
+    );
+
+    this.apollo.watchQuery({
+      query: GET_FILMS
+    }).valueChanges.subscribe(({data, error}: any) => {
+      this.films = data.films;
+      console.log("Film estratti dal server GraphQL: ", data);
+      this.error = error;
+    })
+  }
+
+
+
+}
+
+
+
+
